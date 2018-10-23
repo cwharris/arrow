f2be2acb8562b40559ca03ff3f7d7987f7917398
Christopher Harris <xixonia@gmail.com>
2018-10-23 18:21:25 -0500

    .

--- js/test/Arrow.ts
+++ js/test/Arrow.ts
@@ -50,21 +50,5 @@ else if (!~targets.indexOf(target)) throwInvalidImportError('target', target, ta
 else if (!~formats.indexOf(format)) throwInvalidImportError('module', format, formats);
 else modulePath = path.join(target, format);
 
-// import { read, readAsync } from '../src/Arrow';
-// export { read, readAsync };
-// import { View,  VectorLike } from '../src/Arrow';
-// export { View,  VectorLike };
-// import { Table, Field, Schema, RecordBatch, Type, vector } from '../src/Arrow';
-// export { Table, Field, Schema, RecordBatch, Type, vector };
-
-// import { TypedArray, TypedArrayConstructor, IntBitWidth, TimeBitWidth } from '../src/Arrow';
-// export { TypedArray, TypedArrayConstructor, IntBitWidth, TimeBitWidth };
-
-// type XXX = typeof import('../src/Arrow');
-
-// export namespace Arrow {
-//     export import :XXX;
-// }
-
 export let Arrow: typeof import('../src/Arrow') = require(path.resolve(`./targets`, modulePath, `Arrow`));
 export default Arrow;
